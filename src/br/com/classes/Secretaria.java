471f75eca56213ca4a8fbfa0f549320785be02fb
eclipseCJP <cris.tecwordd@gmail.com>
2022-11-15 17:59:55 -0400

    id incluso na tabela Secretaria

--- src/br/com/classes/Secretaria.java
+++ src/br/com/classes/Secretaria.java
@@ -1,13 +1,19 @@
 package br.com.classes;
 
 public class Secretaria extends PessoaFisica{
-
-    public Secretaria(String nome, char sexo, String rg, String cpf) {
+    private int idSecretaria;
+    public Secretaria(int idSecretaria ,String nome, char sexo, String rg, String cpf) {
         super(nome, sexo, rg, cpf);
     }
 
-    
+    public int getIdSecretaria() {
+        return idSecretaria;
+    }
 
+    public void setIdSecretaria(int idSecretaria) {
+        this.idSecretaria = idSecretaria;
+    }
+    
    /*
     public void mostrarDadosSecretaria() {
         System.out.println("Secretaria------------");
@@ -19,14 +25,13 @@ public class Secretaria extends PessoaFisica{
 */
     @Override
     public void mostrarDadosPessoa() {
-       
+            System.out.println("---------Dados Secretaria------");
+           System.out.println("Codigo:.."+this.getIdSecretaria());
            System.out.println("Secretaria------------");
            System.out.println("Nome:.."+this.getNome());
            System.out.println("Sexo:.."+this.getSexo());
            System.out.println("Rg:.."+this.getRg());
            System.out.println("Cpf:.."+this.getCpf()); 
-        
-        
     }
     
 }
