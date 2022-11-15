f82a877cc54e2c91ff0f9de8dcd5b02fdc274c00
eclipseCJP <cris.tecwordd@gmail.com>
2022-11-15 17:44:07 -0400

    classe cargo modificada

--- src/br/com/classes/Cargo.java
+++ src/br/com/classes/Cargo.java
@@ -3,16 +3,26 @@
 import java.time.LocalDate;
 
 public class Cargo {
+    private int idCargo;
     private String Funcao;
     private float salario;
     private LocalDate dataAdmissao;
     private LocalDate dataDemissao;
 
-    public Cargo(String Funcao, float salario, LocalDate dataAdmissao, LocalDate dataDemissao) {
+    public Cargo(int idCargo,String Funcao, float salario, LocalDate dataAdmissao, LocalDate dataDemissao) {
+        this.idCargo=idCargo;
         this.Funcao = Funcao;
         this.salario = salario;
         this.dataAdmissao = dataAdmissao;
         this.dataDemissao = dataDemissao;
+    }
+
+    public int getIdCargo() {
+        return idCargo;
+    }
+
+    public void setIdCargo(int idCargo) {
+        this.idCargo = idCargo;
     }
 
     public String getFuncao() {
@@ -48,7 +58,8 @@ public class Cargo {
     }
 
     public void mostrarCargo(){
-        
+                System.out.println("---------Dados do Cargo-----------");
+                System.out.println("Codigo:.."+this.getIdCargo());
                 System.out.println("funcao:.."+this.getFuncao());
                 System.out.println("salario:.."+this.getSalario());
                 System.out.println("admissao:.."+this.getDataAdmissao());
