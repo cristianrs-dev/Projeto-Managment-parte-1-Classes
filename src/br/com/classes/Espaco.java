8ddae0bee5f860b0ad43d7530d8823b1ecf76976
eclipseCJP <cris.tecwordd@gmail.com>
2022-11-15 17:27:20 -0400

    ids e chave estranheira acrescentada na classe Espaco

--- src/br/com/classes/Espaco.java
+++ src/br/com/classes/Espaco.java
@@ -4,23 +4,33 @@ import java.time.LocalDate;
 
 public class Espaco {
     private int idEspaco;
-    private String[] descricao;
+    private String descricao;
     private LocalDate horaInicio;
     private LocalDate horaTermino;
+    private int idCondominio;
 
-
-    public Espaco(int idEspaco, String[] descricao, LocalDate horaInicio, LocalDate horaTermino) {
+    public Espaco(int idEspaco, String descricao, LocalDate horaInicio, LocalDate horaTermino, int idCondominio) {
         this.idEspaco = idEspaco;
         this.descricao = descricao;
         this.horaInicio = horaInicio;
         this.horaTermino = horaTermino;
+        this.idCondominio = idCondominio;
     }
 
-    public String[] getDescricao() {
+    public int getIdEspaco() {
+        return idEspaco;
+    }
+
+    public void setIdEspaco(int idEspaco) {
+        this.idEspaco = idEspaco;
+    }
+
+    
+    public String getDescricao() {
         return descricao;
     }
 
-    public void setDescricao(String[] descricao) {
+    public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
 
@@ -39,8 +49,20 @@ public class Espaco {
     public void setHoraTermino(LocalDate horaTermino) {
         this.horaTermino = horaTermino;
     }
+
+    public int getIdCondominio() {
+        return idCondominio;
+    }
+
+    public void setIdCondominio(int idCondominio) {
+        this.idCondominio = idCondominio;
+    }
+    
     
     public void mostrarEspaco(){
+        System.out.println("----------Espaco Interno------------");
+        System.out.println("Condominio:.."+this.getIdCondominio());
+        System.out.println("codigo:.."+this.getIdEspaco());
         System.out.println("descricao:.."+this.getDescricao());
         System.out.println("inicio:.."+this.getHoraInicio());
         System.out.println("termino:.."+this.getHoraTermino());
